3ac0efed5b64b8ce2008ee6867525b1401b4b6f8
Selim Dincer <wowselim@live.de>
2016-10-01 17:00:15 +0200

    load server url from cfg file

--- gscrot-jimghost/src/main/java/co/selim/gscrot/jimghost/JavaImageHost.java
+++ gscrot-jimghost/src/main/java/co/selim/gscrot/jimghost/JavaImageHost.java
@@ -2,7 +2,10 @@ package co.selim.gscrot.jimghost;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Base64;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
 
@@ -10,6 +13,14 @@ import jodd.http.HttpRequest;
 import jodd.http.HttpResponse;
 
 public class JavaImageHost {
+	private static Properties HOST_PROPERTIES = new Properties();
+	static {
+		try {
+			HOST_PROPERTIES.load(new FileInputStream(new File("gscrot-jimghost.cfg")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static String upload(BufferedImage image) throws Exception {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -20,7 +31,7 @@ public class JavaImageHost {
 	}
 
 	public static String upload(byte[] b) throws Exception {
-		HttpResponse response = HttpRequest.post("http://localhost:1337/img/upload")
+		HttpResponse response = HttpRequest.post(HOST_PROPERTIES.getProperty("url"))
 				.body(Base64.getEncoder().encodeToString(b)).send();
 
 		return response.bodyText();
